59dd0683908ea122bd32ba76bfb9f6507ca3dae9
Gerolf Ziegenhain <g@ziegenhain.com>
2015-03-04 18:20:43 +0100

    both servers

--- src/node.c
+++ src/node.c
@@ -4,6 +4,9 @@
 
 extern unsigned char _adc_reg;
 
+//#define SERVER_MRX // FIXME: Server addresses
+#define SERVER_KOB
+
 // FIXME
 void
 mtc(struct node *c)
@@ -11,12 +14,19 @@ mtc(struct node *c)
 	char id[] = "DG6FL/AVR"; // FIXME: this is magic
 
 	snprintf(c->myid, 128, "%s", id);
+#ifdef SERVER_MRX
 	c->ipaddr[0] = 108; // FIXME: this is magic
 	c->ipaddr[1] = 22; // FIXME: this is magic
 	c->ipaddr[2] = 91; // FIXME: this is magic
 	c->ipaddr[3] = 71; // FIXME: this is magic
+#endif
+#ifdef SERVER_KOB
+    c->ipaddr[0] = 192; // FIXME: this is magic
+    c->ipaddr[1] = 94; // FIXME: this is magic
+    c->ipaddr[2] = 73; // FIXME: this is magic
+    c->ipaddr[3] = 9; // FIXME: this is magic
 //mtc-kob.dyndns.org
-// 192.94.73.9
+#endif
 }
 
 
@@ -26,5 +36,5 @@ setnode(struct node *c)
 {
 	c->ch = 33;
 	c->port = 7890;
-        mtc(c);
+    mtc(c);
 }
